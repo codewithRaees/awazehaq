71ad6d0a92c64aa799918555756f9895d564c674
Your Name <hafizraiskundi@gmail.com>
2024-12-17 13:00:52 +0500

    commit json file test

---
+++ SupportersChain.js
@@ -0,0 +1,55 @@
+const SupportersChain = [
+    {
+        "image":"rais.jpg",
+        "name": "XyZ",
+        "profession":"profession Title"
+    },
+    {
+        "image": "s2.jpg",
+        "name": "XyZ",
+        "profession":"profession Title"
+    },
+    {
+        "image": "s1.jpg",
+        "name": "XyZ",
+        "profession":"profession Title"
+    },
+    {
+        "image": "s1.jpg",
+        "name": "XyZ",
+        "profession":"profession Title"
+    },
+    {
+        "image": "s2.jpg",
+        "name": "XyZ",
+        "profession":"profession Title"
+    },
+    {
+        "image": "s3.jpg",
+        "name": "XyZ",
+        "profession":"profession Title"
+    },
+    {
+        "image": "s4.jpg",
+        "name": "XyZ",
+        "profession":"profession Title"
+    },
+    {
+        "image": "s1.jpg",
+        "name": "XyZ",
+        "profession":"profession Title"
+    },
+    {
+        "image": "s4.jpg",
+        "name": "XyZ",
+        "profession":"profession Title"
+    },
+    {
+        "image": "s2.jpg",
+        "name": "XyZ",
+        "profession":"profession Title"
+    }
+    
+    
+]
+export default SupportersChain
--- src/components/Supporters.jsx
+++ src/components/Supporters.jsx
@@ -4,9 +4,19 @@ import SupportersChain from "../Data/SupportersChain.js";
 
 
 const Supporters = () => {
+  
   const [slide, setSlide] = useState(0);
   const [direction, setDirection] = useState(1); // 1: forward, -1: backward
+  const [data, setData] = useState([]);
+ 
 
+  useEffect(() => {
+    fetch("https://raw.githubusercontent.com/yourusername/repository/main/SupportersChain.js")
+      .then((response) => response.json())
+      .then((data) =>  console.log(data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+   console.log(data)
   // Update slide to next or previous based on direction
   const nextslide = () => {
     if (SupportersChain.length - 3 === slide) {
@@ -40,6 +50,10 @@ const Supporters = () => {
 
   return (
     <div className='max-w-screen-lg mx-auto py-5 flex flex-col px-3 md:px-0'>
+      <div>
+      <h1>Content Management</h1>
+     
+    </div>
       <div className="sliderTop flex justify-between px-1 py-5 items-center">
         <h1 className='text-xl md:text-2xl font-extrabold '>Our Supporters & Contributers</h1>
         <div className="arrows flex gap-3 duration-300">
@@ -57,9 +71,11 @@ const Supporters = () => {
           </div>
         </div>
       </div>
+     
       <div className="card-main flex overflow-hidden justify-start">
         {/* Start of Card */}
-        {SupportersChain.map((supporter, index) => (
+        {data.map((supporter, index) => (
+          
           <div
             key={index}
             className="flex flex-col gap-1 md:gap-6 px-3"
